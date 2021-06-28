6b24fefd65f968804d46004309e478afc1035f9b
Jet <Jianquan.xie@infinitypath.com.au>
2021-06-28 14:16:02 +1000

    changes

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
-
+const sql = require('mssql');
 const app = express();
+const dotenv = require('dotenv').config();
+
+const config = {
+    user: process.env.user,
+    password: process.env.password,
+     server: process.env.server,
+    database: process.env.database,
+    options: {
+      cryptoCredentialsDetails: {
+      minVersion: 'TLSv1'
+      },
+      trustServerCertificate: true
+      }
+
+  };
+
+  (async () => {
+    try{
+      await sql.connect(config);
+      console.log("connecting succefully")
+    }catch(err){
+      console.log(err);
+    }
+  })();
+
 
 
 app.listen(3000, () => console.log('listening at 3000'));
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sql = require('mssql');
+
 const router = express.Router();
 const axios = require('axios');
 
@@ -9,50 +10,53 @@ router.use(express.urlencoded({
 }));
 
 
-
-let config = {
-    user: 'sa',
-    password: '0vODz]84',
-     server: "ivp-srv",
-    database: 'Practice',
-    options: {
-      cryptoCredentialsDetails: {
-      minVersion: 'TLSv1'
-      },
-      trustServerCertificate: true
-      }
-
-  };
-
-  (async () => {
-    try{
-      await sql.connect(config);
-      // const result = await sql.query `select surname from dbo.Addresses where ID = 1059`;
-      // console.log(result);
-      console.log("connecting succefully")
-    }catch(err){
-      console.log(err);
-    }
-  })();
-
-
 router.get("/pdf/:PDFName",async (req,res)=> {
+    try{
          const pdfName = req.params.PDFName;
          //regex format 
-         const petInformation = await sql.query("SELECT PatFirstName, PatDOB, PatSex From DBO.PathologyOrdering where RequestCode = '"+pdfName+"'")
+         const petInformation = await sql.query("SELECT PatFirstName, PatDOB, PatSex From DBO.PathologyOrdering where RequestCode = '"+pdfName+"'");
          let petInfo = petInformation.recordsets[0][0];
         
          let age = new Date();
-         age -= petInfo.PatDOB;
+         const days = Math.floor((age -petInfo.PatDOB)/1000/3600/24%365%30);
+         const months =  Math.floor((age -petInfo.PatDOB)/1000/3600/24%365/30);
+         const years = Math.floor((age -petInfo.PatDOB)/1000/3600/24/365);
+         
     
          let animalName = petInfo.PatFirstName;
         
          let sex = petInfo.PatSex =='M' ? "Male": "Female";
-         console.log(age,animalName,sex);
+         console.log(years +' y',months+ " m",days + " d", animalName,sex);
 
+        const ownerInformation = await sql.query("SELECT Owner, Species, Breed, Desexed FROM DBO.Patients,DBO.PathologyOrdering where Patient# = P2kPatientID and  RequestCode = '"+pdfName+"'");
+        const ownerInfo = ownerInformation.recordsets[0][0];
+        const Owner = ownerInfo.Owner;
+        const Species = ownerInfo.Species;
+        const Breed = ownerInfo.Breed;
+        const Desexed = ownerInfo.Desexed;
+
+        console.log(Owner,Species,Breed,Desexed)
+
+        const ClinicInformation = await sql.query("SELECT Surname,[First Name] as FirstName,[Address Line 1] as Address1, [Address Line 2] as Address2, Suburb, State, Postcode,ExternalClinicCode From DBO.Addresses as A, DBO.pathologyOrdering as P where A.ID = P.P2kAddressId and P.RequestCode = '"+pdfName+"' ");
+        const ClinicInfo = ClinicInformation.recordsets[0][0];
+        const ClinicDetails = ClinicInfo.Address1 ;
+        const Address = ClinicInfo.Address2 + ' ' + ClinicInfo.Suburb +' ,' + ClinicInfo.State +' ' + ClinicInfo.Postcode;
+        const surname = ClinicInfo.Surname;
+        const FirstName = ClinicInfo.FirstName;
+        // let AVA = ClinicInfo.ExternalClinicCode;
+        // const AVANum = AVA.slice(6);
+        console.log(ClinicDetails);
+        console.log(Address);
+    } catch(err) {
+        console.log(err)
+    }
 })
 
 
+router.get("/pdf/:PDFName",async (req,res)=> {
+    const pdfName = req.params.PDFName;
+
+});
 
 router.get('/query', (req,res)=> {
     const name = req.query.name;
@@ -71,8 +75,7 @@ router.post('/connect',(req,res)=> {
 
         const body = req.body;
         console.log(body);
-        console.log(pdfName);
-        sql.query("insert into dbo.sampleindicator values(2,3,4,'"+body.ClinicalHisotry+"','"+body.description+"','"+body.cytologyFindings+"','"+body.differentialDiag+"', '"+pdfName+"')");
+        // sql.query("insert into dbo.sampleindicator values(2,3,4,'"+body.ClinicalHisotry+"','"+body.description+"','"+body.cytologyFindings+"','"+body.differentialDiag+"', '"+pdfName+"')");
 
         res.json({
             confirmation:"success",
@@ -85,15 +88,13 @@ router.post('/connect',(req,res)=> {
 router.put('/pdf/:id', (request, response) => {
     const id = request.params.id;
  
-    pool.query('UPDATE dbo.sampleindicator SET ? WHERE id = ?', [request.body, id], (error, result) => {
+    sql.query('UPDATE dbo.sampleindicator SET ? WHERE id = ?', [request.body, id], (error, result) => {
         if (error) throw error;
- 
         response.send('User updated successfully.');
     });
 });
 
 
-   
 
 
 module.exports = router;
