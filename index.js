2cba492c3105d3c45cfd1d05e9129bd26a12f0ad
Jet <Jianquan.xie@infinitypath.com.au>
2021-07-16 15:15:13 +1000

    Access-Control-Allow-Methods

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ app.use(express.json({ limit: "2mb" }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header({
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS",
+  });
   next();
 });
 
